9a9d7205e38d0e8d052c59de259bc3545932f7a2
Zoey <zoey@panties.moe>
2022-04-07 12:41:22 -0400

    add resources folder

---
+++ resources/themes.js
@@ -0,0 +1,24 @@
+const oObjectFreeze = this.Object.freeze;
+Object.freeze = function(obj, ...args) {
+  if (!obj?.hasOwnProperty) { 
+      return oObjectFreeze.apply(this, [obj, ...args])
+  } 
+  try { 
+    const theme = %@;
+    if (obj.hasOwnProperty?.('BACKGROUND_PRIMARY')) {
+      return oObjectFreeze.apply(this, [{
+        ...obj,
+        ...theme.theme_color_map
+      }, ...args]);
+    }
+    if (obj.hasOwnProperty?.('PRIMARY_DARK')) {
+      return oObjectFreeze.apply(this, [{
+        ...obj,
+        ...theme.colours
+      }, ...args]);
+    }
+  } catch(e) {
+    console.log(e);
+  }
+  return oObjectFreeze.apply(this, [obj, ...args]);
+}
