9d1fa86662ce9c6d4698d0cf116a76b4b1894c9e
Zoey <zoey@panties.moe>
2022-04-10 17:39:44 -0400

    fix themes.js

--- Resources/themes.js
+++ Resources/themes.js
@@ -21,10 +21,14 @@ Object.freeze = function(obj, ...args) {
     }
 
     if (obj.hasOwnProperty?.('CHAT_GREY')) {
-      return oObjectFreeze.apply(this, [{
-        ...obj,
-        'CHAT_GREY': theme.colours.PRIMARY_DARK_630,
-      }, ...args]);
+      if (themes?.colours?.PRIMARY_DARK_630) {
+        return oObjectFreeze.apply(this, [{
+          ...obj,
+          'CHAT_GREY': theme.colours.PRIMARY_DARK_630,
+        }, ...args]);
+      } else {
+        return oObjectFreeze.apply(this, [obj, ...args]);
+      }
     }
   } catch(e) {
     console.log(e);
