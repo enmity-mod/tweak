62fd108ce506d073d14d753655f1c873b754ef77
Zoey <zoey@panties.moe>
2022-04-10 17:29:25 -0400

    cleanup themes.js

--- Resources/themes.js
+++ Resources/themes.js
@@ -1,30 +1,34 @@
 const oObjectFreeze = this.Object.freeze;
 Object.freeze = function(obj, ...args) {
   if (!obj?.hasOwnProperty) { 
-      return oObjectFreeze.apply(this, [obj, ...args])
-  } 
+    return oObjectFreeze.apply(this, [obj, ...args])
+  }
   try { 
     const theme = %@;
+
     if (obj.hasOwnProperty?.('BACKGROUND_PRIMARY')) {
       return oObjectFreeze.apply(this, [{
         ...obj,
         ...theme.theme_color_map
       }, ...args]);
     }
+
     if (obj.hasOwnProperty?.('PRIMARY_DARK')) {
       return oObjectFreeze.apply(this, [{
         ...obj,
         ...theme.colours
       }, ...args]);
     }
+
     if (obj.hasOwnProperty?.('CHAT_GREY')) {
       return oObjectFreeze.apply(this, [{
         ...obj,
-        'CHAT_GREY': theme.colours.PRIMARY_DARK_600,
+        'CHAT_GREY': theme.colours.PRIMARY_DARK_630,
       }, ...args]);
     }
   } catch(e) {
     console.log(e);
   }
+
   return oObjectFreeze.apply(this, [obj, ...args]);
 }
