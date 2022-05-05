47fa89a35ad6e755f81f272569a0b6e31ea6a913
Zoey <zoey@panties.moe>
2022-05-05 16:21:46 -0400

    better handling of bleeding-edge

--- src/Utils.h
+++ src/Utils.h
@@ -6,6 +6,14 @@
 #define REMOTE_HASH @"https://files.enmity.app/hash"
 #define BUNDLE_PATH @"/Library/Application Support/Enmity/EnmityFiles.bundle"
 
+#if BLEEDING_EDGE == 1
+#   define IS_BLEEDING_EDGE true
+#   define ENMITY_URL @"https://raw.githubusercontent.com/enmity-mod/enmity/main/dist/Enmity.js"
+#else 
+#   define IS_BLEEDING_EDGE false
+#   define ENMITY_URL @"https://files.enmity.app/Enmity.js"
+#endif
+
 NSString* getDownloadURL();
 BOOL checkForUpdate();
 NSString* getHash(NSString *url);
