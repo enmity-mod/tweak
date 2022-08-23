aaed3d1d2d25600ed14225e149ee72fe8ba055a0
eternal <eternal.git@gmail.com>
2022-08-23 05:02:47 +0100

    change casing because yes

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,32 @@
-import { defineConfig } from "rollup";
-import esbuild from "rollup-plugin-esbuild";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import esbuild from 'rollup-plugin-esbuild';
+import { defineConfig } from 'rollup';
 
 function addWindowDeclaration() {
   return {
-    name: "window-is-this",
+    name: 'window-is-this',
     generateBundle: (_, bundle) => {
       for (const file in bundle) {
-        bundle[file].code = "const window = this;" + bundle[file].code;
+        bundle[file].code = 'const window = this;' + bundle[file].code;
       }
     }
   }
 }
 
 export default defineConfig({
-  input: "devtools.js",
+  input: 'devtools.js',
   output: [
     {
-      file: "Resources/devtools.js",
-      format: "cjs",
+      file: 'resources/devtools.js',
+      format: 'cjs',
       strict: false
     },
   ],
   plugins: [
     nodeResolve(),
     commonjs(),
-    esbuild({ minify: true, target: "ES2019" }),
+    esbuild({ minify: true, target: 'ES2019' }),
     addWindowDeclaration()
   ]
 });
